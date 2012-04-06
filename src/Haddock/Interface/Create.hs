ghcjs/haddock-internal

Fix reporting of modules safe haskell mode (#5989)

src/Haddock/Interface/Create.hs
@@ -45,6 +45,7 @@ createInterface tm flags modMap instIfaceMap = do
 
   let ms            = pm_mod_summary . tm_parsed_module $ tm
       mi            = moduleInfo tm
+      safety        = modInfoSafe mi
       mdl           = ms_mod ms
       dflags        = ms_hspp_opts ms
       instances     = modInfoInstances mi
@@ -68,7 +69,7 @@ createInterface tm flags modMap instIfaceMap = do
         | Flag_IgnoreAllExports `elem` flags = OptIgnoreExports : opts0
         | otherwise = opts0
 
-  (info, mbDoc) <- liftErrMsg $ lexParseRnHaddockModHeader dflags gre mayDocHeader
+  (info, mbDoc) <- liftErrMsg $ lexParseRnHaddockModHeader dflags gre safety mayDocHeader
 
   let declsWithDocs = topDecls group_
       (decls, _) = unzip declsWithDocs

src/Haddock/Interface/LexParseRn.hs
@@ -67,8 +67,9 @@ lexParseRnMbHaddockComment dflags hty gre (Just d) = lexParseRnHaddockComment df
 
 
 -- yes, you always get a HaddockModInfo though it might be empty
-lexParseRnHaddockModHeader :: DynFlags -> GlobalRdrEnv -> GhcDocHdr -> ErrMsgM (HaddockModInfo Name, Maybe (Doc Name))
-lexParseRnHaddockModHeader dflags gre mbStr = do
+lexParseRnHaddockModHeader :: DynFlags -> GlobalRdrEnv -> SafeHaskellMode -> GhcDocHdr
+                           -> ErrMsgM (HaddockModInfo Name, Maybe (Doc Name))
+lexParseRnHaddockModHeader dflags gre safety mbStr = do
   (hmi, docn) <-
     case mbStr of
       Nothing -> return failure
@@ -79,9 +80,8 @@ lexParseRnHaddockModHeader dflags gre mbStr = do
             tell ["haddock module header parse failed: " ++ mess]
             return failure
           Right (info, doc) -> return (renameHmi gre info, Just (rename gre doc))
-  return (hmi { hmi_safety = safety }, docn)
+  return (hmi { hmi_safety = Just $ showPpr safety }, docn)
   where
-    safety  = Just $ showPpr $ safeHaskell dflags
     failure = (emptyHaddockModInfo, Nothing)
 
 

telser/pollock

src/Haddock/Interface/Create.hs
@@ -47,6 +47,7 @@ createInterface tm flags modMap instIfaceMap = do
 
   let ms            = pm_mod_summary . tm_parsed_module $ tm
       mi            = moduleInfo tm
+      safety        = modInfoSafe mi
       mdl           = ms_mod ms
       dflags        = ms_hspp_opts ms
       instances     = modInfoInstances mi
@@ -69,7 +70,7 @@ createInterface tm flags modMap instIfaceMap = do
         | otherwise = opts0
 
   (info, mbDoc) <- do
-    (i, d) <- liftErrMsg $ processModuleHeader dflags gre mayDocHeader
+    (i, d) <- liftErrMsg $ processModuleHeader dflags gre safety mayDocHeader
     return (i, addModuleWarning warnings d)
 
   let declsWithDocs = topDecls group_

src/Haddock/Interface/LexParseRn.hs
@@ -64,8 +64,9 @@ process parse dflags gre (HsDocString fs) = do
      Just doc -> return (Just (rename gre doc))
 
 
-processModuleHeader :: DynFlags -> GlobalRdrEnv -> Maybe LHsDocString -> ErrMsgM (HaddockModInfo Name, Maybe (Doc Name))
-processModuleHeader dflags gre mayStr = do
+processModuleHeader :: DynFlags -> GlobalRdrEnv -> SafeHaskellMode -> Maybe LHsDocString
+                    -> ErrMsgM (HaddockModInfo Name, Maybe (Doc Name))
+processModuleHeader dflags gre safety mayStr = do
   (hmi, doc) <-
     case mayStr of
       Nothing -> return failure
@@ -79,8 +80,7 @@ processModuleHeader dflags gre mayStr = do
             let hmi' = hmi { hmi_description = rename gre <$> hmi_description hmi }
                 doc' = rename gre doc
             return (hmi', Just doc')
-  let safety = Just $ showPpr $ safeHaskell dflags
-  return (hmi { hmi_safety = safety }, doc)
+  return (hmi { hmi_safety = Just $ showPpr safety }, doc)
   where
     failure = (emptyHaddockModInfo, Nothing)
 